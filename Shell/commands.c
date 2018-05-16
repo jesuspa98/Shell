cf60545ff7d22701906965627ee304c53595dd38
jesuspa98 <jesusparejo98@gmail.com>
2018-05-16 14:17:02 +0200

    Modified command.c

--- Shell/commands.c
+++ Shell/commands.c
@@ -10,7 +10,6 @@ static const ShellCommands shell_commands[] = { {"cd"} };
 int isAShellOrder(char* command_name, char* args[]){
 	int equals = 0, i = 0;
 	size_t length = sizeof(shell_commands)/sizeof(shell_commands[0]);
-
 	while(!equals && i < length){
 		equals = (strcmp(command_name, shell_commands[i].name));
 		i++;
